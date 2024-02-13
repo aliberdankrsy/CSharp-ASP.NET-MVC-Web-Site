046a3ac1450a20d171f3beb4e14805635ba9347a
Ali Berdan Karasoy <aliberdankrsy@outlook.com>
2024-02-13 12:43:38 +0300

    C# ASP.NET MVC Web Site
    Asp.Net MVC alışveriş sitesi

---
+++ C# ASP.NET MVC Web Site/wwwroot/lib/twitter-bootstrap/js/jsdosyam.js
@@ -0,0 +1,15 @@
+﻿document.getElementById('changeColorButton').addEventListener('click', function () {
+    document.body.style.backgroundColor = getRandomColor();
+    document.body.classList.remove('patternarkaplan'); // arka plan rengi değişeceği zaman arkaplan resminin olduğu class ı programdan çıkartıyoruz
+});
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+//Rastgele arka plan rengi değiştirme
